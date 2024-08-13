6dbde8ad30909f8f25be06f51ebe981c46c05d0d
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-08-13 21:19:07 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,104 @@
+
+const { ApolloServer } = require('@apollo/server');
+const express = require('express')
+
+// import { ApolloServer } from '@apollo/server';
+const { startStandaloneServer } = require('@apollo/server/standalone');
+
+const resolvers = {
+    Query: {
+        books: () => books,
+        getPosts: () => posts
+    }
+}
+
+
+
+let books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+
+
+let posts = [{
+        "postId": 1,
+        "id": 1,
+        "name": "id labore ex et quam laborum",
+        "email": "Eliseo@gardner.biz",
+        "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+    },
+    {
+        "postId": 1,
+        "id": 2,
+        "name": "quo vero reiciendis velit similique earum",
+        "email": "Jayne_Kuhic@sydney.com",
+        "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+    },
+    {
+        "postId": 1,
+        "id": 3,
+        "name": "odio adipisci rerum aut animi",
+        "email": "Nikita@garfield.biz",
+        "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+    },
+    {
+        "postId": 1,
+        "id": 4,
+        "name": "alias odio sit",
+        "email": "Lew@alysha.tv",
+        "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+    },
+    {
+        "postId": 1,
+        "id": 5,
+        "name": "vero eaque aliquid doloribus et culpa",
+        "email": "Hayden@althea.biz",
+        "body": "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+    },
+]
+
+
+
+
+
+
+const typeDefs = `
+    #graphql
+
+    type Book {
+        title: String
+        author: String
+    }
+    type Post {
+        postId: ID
+        id: Int
+        name: String
+        email: String
+        body: String
+    }
+
+    type Query {
+        books: [Book]
+        getPosts: [Post]
+    }
+
+`
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+
+const { url } = startStandaloneServer(server, {
+    listen: {
+        port: 9000,
+    }
+})
+
